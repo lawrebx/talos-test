a20af536f7e3d387c9a0f1e4f40bf36543a63619
Brandon Lawrence <36019489+lawrebx@users.noreply.github.com>
2024-03-07 15:30:47 +0000

    edits

--- pages/1Executive Summary.py
+++ pages/1Executive Summary.py
@@ -1,9 +1,9 @@
 import streamlit as st
 import pandas as pd
 from streamlit_folium import st_folium
-from area_map import gen_area_map
+from portfolio_map import gen_portfolio_map
 
-st.set_page_config(page_title="Talos - Executive Summary",page_icon=':monocle_face:',layout="wide")
+st.set_page_config(page_title="Talos - Executive Summary",page_icon=':dollar:',layout="wide")
 
 area_dayparts = ['Morning Commute','Lunch Rush','Evening Commute','Overnight']
 sales_source = pd.read_csv('sales_data.csv')
@@ -16,6 +16,6 @@ with st.sidebar:
 
 st.divider()
 
-st_folium(gen_area_map(33.180382,-97.100601, 'x'), width = 925)
+st_folium(gen_portfolio_map(33.180382,-97.100601, 'x'), width = 925)
 
 st.divider()
