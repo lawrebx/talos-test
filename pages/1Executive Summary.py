0c8a7844999ccb8f8bbd64683b272d3304571188
Brandon Lawrence <36019489+lawrebx@users.noreply.github.com>
2024-03-20 02:37:11 +0000

    s

--- pages/1Executive Summary.py
+++ pages/1Executive Summary.py
@@ -5,17 +5,4 @@ from portfolio_map import gen_portfolio_map
 
 st.set_page_config(page_title="Talos - Executive Summary",page_icon=':dollar:',layout="wide")
 
-area_dayparts = ['Morning Commute','Lunch Rush','Evening Commute','Overnight']
-sales_source = pd.read_csv('sales_data.csv')
-all_site_data = pd.read_csv('site_data.csv')
-
-with st.sidebar:
-    st.image('images/Talos Logo - High Res - White Text.png',use_column_width=True)
-    daypart_selection = st.selectbox('Daypart',area_dayparts)
-    product_selection = st.selectbox('Product',set(sales_source['Product']))
-
-st.divider()
-
-st_folium(gen_portfolio_map(33.180382,-97.100601, 'x'), width = 925)
-
-st.divider()
+st.header('Coming Soon!')
--- pages/2Area Analysis.py
+++ pages/2Area Analysis.py
@@ -5,17 +5,4 @@ from area_map import gen_area_map
 
 st.set_page_config(page_title="Talos - Area Dashboard",page_icon=':briefcase:',layout="wide")
 
-area_dayparts = ['Morning Commute','Lunch Rush','Evening Commute','Overnight']
-sales_source = pd.read_csv('sales_data.csv')
-all_site_data = pd.read_csv('site_data.csv')
-
-with st.sidebar:
-    st.image('images/Talos Logo - High Res - White Text.png',use_column_width=True)
-    daypart_selection = st.selectbox('Daypart',area_dayparts)
-    product_selection = st.selectbox('Product',set(sales_source['Product']))
-
-st.divider()
-
-st_folium(gen_area_map(33.180382,-97.100601, 'x'), width = 925)
-
-st.divider()
+st.header('Coming Soon!')
