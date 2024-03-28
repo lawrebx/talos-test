3883c0f9c5ebbeafb3623d2e311f6d1dab108ed4
Brandon Lawrence <36019489+lawrebx@users.noreply.github.com>
2024-03-28 16:27:34 +0000

    reorg

--- pages/1Executive Summary.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-import pandas as pd
-from streamlit_folium import st_folium
-from portfolio_map import gen_portfolio_map
-
-st.set_page_config(page_title="Talos - Executive Summary",page_icon=':dollar:',layout="wide")
-
-st.header('Coming Soon!')
